5d774353d9586d39f2e36887f8ec2c32e66c1149
soham <sohamshere@yahoo.com>
2026-09-16 06:35:46 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Exam Portal</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/services/api.js
@@ -0,0 +1,34 @@
+const API_URL = "http://localhost:5000/api";
+
+const apiRequest = async (endpoint, options = {}) => {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+        "Content-Type": "application/json",
+        ...options.headers
+    };
+
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    const response = await fetch(
+        `${API_URL}${endpoint}`,
+        {
+            ...options,
+            headers
+        }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(
+            data.message || "Something went wrong"
+        );
+    }
+
+    return data;
+};
+
+export default apiRequest;
